2af456dad3b63ebc70b5be65af9594c9d12890d8
Yockium <mtfmattye@gmail.com>
2024-12-09 17:29:18 +0100

    m

--- src/entities/Product/ui/ProductCard.tsx
+++ src/entities/Product/ui/ProductCard.tsx
@@ -47,7 +47,7 @@ const ProductCard:React.FC<ProductCardProps> =
                     {rating}
                 </span>
             </div>
-            <h2>
+            <h2>2
                 ${price}
             </h2>
         </article>
